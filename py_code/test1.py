9f5f5247517a7810078dce4c17d8117c119cdf87
leyanlyly <lly19919@163.com>
2021-10-31 20:54:41 +0800

    first commit

---
+++ py_code/test1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 19:38:12 2021
+
+@author: 65125
+"""
+
+import  numpy as np
+
+print("hello world!")
+print("the conda have no problem!")
+#np.random(1,100)
+print(np.random.rand(2,3))
